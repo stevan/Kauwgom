63a36931aaacb1fd85fb87ed94e6095c6b98b274
Stevan Little <stevan.little@gmail.com>
2018-05-22 16:24:18 +0200

    the parser now creates a proper Page object

--- lib/Kauwgom/XML/SAX/Handler.pm
+++ lib/Kauwgom/XML/SAX/Handler.pm
@@ -4,16 +4,23 @@ use v5.24;
 use warnings;
 use experimental 'signatures', 'postderef';
 
+use Kauwgom::Page;
+use Kauwgom::Page::Store;
+use Kauwgom::Page::Store::Provider;
+use Kauwgom::Page::Body;
+use Kauwgom::Page::Component;
+
 our $VERSION = '0.01';
 
 use parent 'UNIVERSAL::Object', 'XML::SAX::Base';
-use slots (
-    _stack  => sub { +[] },
-);
+use slots ( _stack  => sub { +[] } );
 
+## extend a non UNIVESAL::Object class ...
 sub REPR ($class, $proto) {
     return $class->XML::SAX::Base::new( %$proto );
 }
+
+## ...
 
 sub start_document ($self, $) {
     my $d = { name => __PACKAGE__ };
@@ -44,7 +51,73 @@ sub end_element ($self, $)  {
 }
 
 sub end_document ($self, $) {
-    pop $self->{_stack}->@*;
+    my $data = pop $self->{_stack}->@*;
+    my $page = $self->_inflate_page( $data->{children}->[0] );
+    #use Data::Dumper;
+    #warn Dumper $page;
+    return $page;
+}
+
+## ...
+
+sub _inflate_page ($self, $data) {
+
+    my ($store, $server_comps, $client_comps, $body);
+    foreach my $c ( $data->{children}->@* ) {
+        if ( $c->{name} eq 'store' ) {
+
+            my @providers;
+            foreach my $provider ( $c->{children}->@* ) {
+
+                my $name             = $provider->{attributes}->{'name'};
+                my @available_on     = split /\,/ => $provider->{attributes}->{'available-on'};
+                my ($type, $handler) = split /\// => $provider->{attributes}->{'provider'};
+
+
+                my %parameters;
+                foreach my $param ( $provider->{children}->@* ) {
+                    $parameters{ $param->{attributes}->{'from'} } = $param->{attributes}->{'to'};
+                }
+
+                push @providers => Kauwgom::Page::Store::Provider->new(
+                    type         => $type,
+                    handler      => $handler,
+                    name         => $name,
+                    available_on => \@available_on,
+                    parameters   => \%parameters,
+                )
+            }
+
+            $store = Kauwgom::Page::Store->new( providers => \@providers );
+        }
+        elsif ( $c->{name} eq 'server-components' ) {
+            $server_comps = [
+                map
+                Kauwgom::Page::Component->new( $_->{attributes}->%{qw[ type name ]} ),
+                $c->{children}->@*
+            ];
+        }
+        elsif ( $c->{name} eq 'client-components' ) {
+            $client_comps = [
+                map
+                Kauwgom::Page::Component->new( $_->{attributes}->%{qw[ type name ]} ),
+                $c->{children}->@*
+            ];
+        }
+        elsif ( $c->{name} eq 'body' ) {
+            $body = Kauwgom::Page::Body->new( $c->{attributes}->%{qw[ layout header footer ]} );
+        }
+        else {
+            die "No idea what this is: " . Data::Dumper::Dumper( $c );
+        }
+    }
+
+    return Kauwgom::Page->new(
+        store             => $store,
+        server_components => $server_comps,
+        client_components => $client_comps,
+        body              => $body,
+    );
 }
 
 __PACKAGE__;
--- t/100-page-xml.t
+++ t/100-page-xml.t
@@ -8,11 +8,14 @@ use Test::More;
 use XML::SAX::Expat;
 use Kauwgom::XML::SAX::Handler;
 
-my $h = Kauwgom::XML::SAX::Handler->new;
-my $p = XML::SAX::Expat->new( Handler => $h );
-my $r = $p->parse_file('root/app.xml');
+my $x = XML::SAX::Expat->new( Handler => Kauwgom::XML::SAX::Handler->new );
+my $p = $x->parse_file('root/app.xml');
 
-use Data::Dumper;
-warn Dumper $r;
+isa_ok($p, 'Kauwgom::Page');
+isa_ok($p->body, 'Kauwgom::Page::Body');
+isa_ok($p->store, 'Kauwgom::Page::Store');
+
+#use Data::Dumper;
+#warn Dumper $p;
 
 done_testing;
