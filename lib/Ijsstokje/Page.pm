f93408fd2fb47fb40578a0c13efe369679fb7b52
Stevan Little <stevan.little@gmail.com>
2018-05-30 20:05:45 +0200

    no need to have private slots, it is just needless obsfucation

--- lib/Ijsstokje/Page.pm
+++ lib/Ijsstokje/Page.pm
@@ -8,30 +8,22 @@ our $VERSION = '0.01';
 
 use parent 'UNIVERSAL::Object::Immutable';
 use slots (
-    _store => sub {},
-    _comps => sub {},
-    _body  => sub {},
+    store      => sub {},
+    components => sub {},
+    body       => sub {},
 );
 
-sub BUILD ($self, $params) {
-    # TODO
-    # do some type-check-ing here
-    $self->{_store} = $params->{store}      if exists $params->{store};
-    $self->{_body}  = $params->{body}       if exists $params->{body};
-    $self->{_comps} = $params->{components} if exists $params->{components};
-}
-
-sub has_store ($self) { defined $self->{_store} }
-sub store     ($self) {         $self->{_store} }
+sub has_store ($self) { defined $self->{store} }
+sub store     ($self) {         $self->{store} }
 
 sub has_server_components ($self) { !! scalar $self->server_components }
-sub server_components     ($self) { grep $_->env eq 'server', $self->{_comps}->@* }
+sub server_components     ($self) { grep $_->env eq 'server', $self->{components}->@* }
 
 sub has_client_components ($self) { !! scalar $self->client_components }
-sub client_components     ($self) { grep $_->env eq 'client', $self->{_comps}->@* }
+sub client_components     ($self) { grep $_->env eq 'client', $self->{components}->@* }
 
-sub has_body ($self) { defined $self->{_body} }
-sub body     ($self) {         $self->{_body} }
+sub has_body ($self) { defined $self->{body} }
+sub body     ($self) {         $self->{body} }
 
 __PACKAGE__;
 
